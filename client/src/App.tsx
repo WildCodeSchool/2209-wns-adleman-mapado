d425a38ada4fc0eac25d90e13094747cf47464f3
Anna <68162650+AnnaGosme@users.noreply.github.com>
2023-01-27 14:34:27 +0100

    Revert "Client components"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,96 +1,44 @@
-import { Route, Routes } from "react-router-dom";
-import {gql, useQuery} from '@apollo/client';
-import CSS from 'csstype';
-import Home from "./screens/Home";
-import Header from './components/Header';
-import AddManageCities from './components/AddManageCities';
+import React from "react";
 import "./App.css";
-// import bordeaux from "./assets/bordeaux.jpeg";
-// import lille from "./assets/lille.jpeg";
-// import paris from "./assets/paris.jpeg";
+import {gql, useQuery} from '@apollo/client';
+import binoculars from "./assets/images/binculars.png"
 
+const GET_CITIES = gql`
+query Cities {
+  cities {
+    id
+    name
+  }
+}
+`
 
-const styles: CSS.Properties = {
-  margin: 0,
-  backgroundSize: 'cover',
-  backgroundColor: '#3270F4',
-};
-
-//MOCK DATA CITIES
-
-// const cities: {
-//   id: number;
-//   name: string;
-//   city_area: string;
-//   photo?: string;
-//   user: {}[];
-// }[] = [
-//   {
-//     id: 1,
-//     name: "Bordeaux",
-//     city_area: "44.8473900254569, -0.7188799313856008",
-//     photo: bordeaux,
-//     user: ["1", "2", "3"],
-//   },
-//   {
-//     id: 2,
-//     name: "Lille",
-//     city_area: "50.62976866778606, 3.04621070309212",
-//     photo: lille,
-//     user: ["4", "5", "6"],
-//   },
-//   {
-//     id: 3,
-//     name: "Paris",
-//     city_area: "48.87170691129766, 2.325403812427441",
-//     photo: paris,
-//     user: ["7", "8", "9"],
-//   },
-//   {
-//     id: 4,
-//     name: "Marseille",
-//     city_area: "43.332225160179654, 5.398493055776797",
-//     user: ["10", "11", "12"],
-//   },
-// ];
-
-interface City {
-  id: number;
-  name: string;
-  city_area: string;
-  photo?: string;
-  user: {}[];
+interface Cities {
+    id: number;
+    name: string;
 }
 
-
 function App() {
+    const {data} = useQuery<{ cities: Cities[] }>(GET_CITIES);
+    console.log(data)
 
-  const GET_CITIES = gql`
-  query Cities {
-    cities {
-      id
-      name
-      city_area
-      photo
-      user
-    }
-  }
-  `
+    const cities = data?.cities || []
 
-const {data} = useQuery<{ cities: City[] }>(GET_CITIES);
-console.log(data)
+    return (
+        <div className="App">
+            <div className={"title"}>
+                <h1>
+                    Mapado
+                </h1>
+                <img src={binoculars} alt={"binoculars"}/>
+            </div>
 
-const cities = data?.cities || []
-
-  return (
-    <div style={styles}>
-      <Header />
-      <Routes>
-      <Route path="/" element={<Home cities={cities} />} />
-      <Route path="/manage-cities" element={<AddManageCities cities={cities}/>} />
-      </Routes>
-    </div>
-  );
+            <div className={"cities"}>
+                {cities.map((c) =>
+                    <h1 key={c.id} className={"city"}>{c.name}</h1>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- server/src/entity/City.ts
+++ server/src/entity/City.ts
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import User from "./User";
-
-@Entity()
-
-class City {
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column()
-    User_id: number;
-
-    @Column()
-    name: string;
-
-    @Column({ nullable: true, length: 100 })
-    Photo: string;
-
-    @Column()
-    City_area: string;
-
-    @Column()
-    userId: number;
-
-    @OneToMany(() => User, (u) => u.city)
-    user: User;
-}
-
-export default City;
-=======
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 import {Field, InputType, ObjectType} from "type-graphql";
 
@@ -49,4 +18,3 @@ export class City {
     @Field()
     name: string;
 }
->>>>>>> origin
