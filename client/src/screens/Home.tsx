b72c8f5761f43583c52ddab2c15b619a8c92062b
ggors <ggorski@consoneo.com>
2023-02-01 12:50:48 +0100

    🚧 WIP

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,4 +1,3 @@
-import CSS from "csstype";
 import CityCard from "../components/CityCard";
 
 interface City {
@@ -14,33 +13,14 @@ interface Cities {
 }
 
 export default function Home({ cities }: Cities) {
-  const styles: CSS.Properties = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    margin: "2rem",
-  };
-  const addCityButtonStyles: CSS.Properties = {
-    height: "17rem",
-    width: "15.6rem",
-    border: "10px solid #EC5D5C",
-    borderRadius: "40px",
-    backgroundColor: "#FFFFFF",
-    margin: "2rem",
-    color: "#EC5D5C",
-    fontFamily: "Josefin Sans",
-    fontWeight: 700,
-    fontSize: "1.25rem",
-  };
 
   return (
-    <div style={styles}>
+    <div className={"home_container"}>
       <a href="/manage-cities">
-        <button style={addCityButtonStyles}>
+        <button className={"addCityButton"}>
           <p>AJOUTER UNE VILLE</p>
         </button>
       </a>
-
       {cities.map((city: City) => {
         return (
           <CityCard key={city.id} cityName={city.name} cityPhoto={city.photo} />
