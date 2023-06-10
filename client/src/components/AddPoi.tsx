fe011d152f007cde37af51dff3fb069b5d62d8db
ggors <ggorski@consoneo.com>
2023-06-10 14:14:17 +0200

    🐛 Fix bugs to start app

--- client/src/components/AddPoi.tsx
+++ client/src/components/AddPoi.tsx
@@ -1,112 +1,112 @@
-import { useState } from "react";
+import {useState} from "react";
 import {
-  useFetchPoiCoordinatesMutation,
-  useGetCityQuery,
+    useFetchPoiCoordinatesMutation,
+    useGetCityQuery,
 } from "../gql/generated/schema";
-import { ApolloError } from "@apollo/client";
+import {ApolloError} from "@apollo/client";
 import checkIcon from "../assets/svg/check.svg";
 import errorIcon from "../assets/svg/error.svg";
 import Toast from "./Toast";
 
 interface PoiProps {
-  cityId: number;
-  cityName: string;
+    cityId: number;
+    cityName: string;
 }
 
 interface PoiRequestedInterface {
-  poiNameOrAdress: string;
-  cityId: number;
-  cityName: string;
+    poiNameOrAdress: string;
+    cityId: number;
+    cityName: string;
 }
 
 interface ToastInterface {
-  id: number;
-  title: string;
-  description: string;
-  icon: string;
-  backgroundColor: string;
+    id: number;
+    title: string;
+    description: string;
+    icon: string;
+    backgroundColor: string;
 }
 
-export default function AddPoi({ cityId, cityName }: PoiProps) {
-  // Initialisation de l'objet poiRequested
-  const [poiRequested, setPoiRequested] = useState<PoiRequestedInterface>({
-    poiNameOrAdress: "",
-    cityId: 0,
-    cityName: "",
-  });
+export default function AddPoi({cityId, cityName}: PoiProps) {
+    // Initialisation de l'objet poiRequested
+    const [poiRequested, setPoiRequested] = useState<PoiRequestedInterface>({
+        poiNameOrAdress: "",
+        cityId: 0,
+        cityName: "",
+    });
 
-  // Initialisation de l'objet Toast
-  const [toastData, setToastData] = useState<ToastInterface>({
-    id: 0,
-    title: "",
-    description: "",
-    icon: "",
-    backgroundColor: "",
-  });
-  const [showToast, setShowToast] = useState(false);
+    // Initialisation de l'objet Toast
+    const [toastData, setToastData] = useState<ToastInterface>({
+        id: 0,
+        title: "",
+        description: "",
+        icon: "",
+        backgroundColor: "",
+    });
+    const [showToast, setShowToast] = useState(false);
 
-  const [sendPoiNameOrAddress] = useFetchPoiCoordinatesMutation();
+    const [sendPoiNameOrAddress] = useFetchPoiCoordinatesMutation();
 
-  const { refetch } = useGetCityQuery({
-    variables: { query: cityName! },
-  });
+    const {refetch} = useGetCityQuery({
+        variables: {query: cityName!},
+    });
 
-  const onClickSendNewPoi = () => {
-    sendPoiNameOrAddress({ variables: { data: poiRequested } })
-      .then((res) => {
-        /* console.log("log du then", res); */
+    const onClickSendNewPoi = () => {
+        sendPoiNameOrAddress({variables: {data: poiRequested}})
+            .then((res) => {
+                /* console.log("log du then", res); */
 
-        setToastData({
-          id: Math.floor(Math.random() * 100 + 1),
-          description: res?.data?.fetchPoiCoordinates!,
-          title: "Super ! 👍",
-          backgroundColor: "green",
-          icon: checkIcon,
-        });
-        /*  console.log("log de toastData au click après le set", toastData); */
-      })
-      .catch((erreur: ApolloError) => {
-        /* console.log(erreur); */
-        setToastData({
-          id: Math.floor(Math.random() * 100 + 1),
-          description: erreur.message,
-          title: "Oups... 🧐",
-          backgroundColor: "#bd2424",
-          icon: errorIcon,
-        });
-      })
-      .finally(() => {
-        setShowToast(true);
-        refetch();
-      });
-  };
+                setToastData({
+                    id: Math.floor(Math.random() * 100 + 1),
+                    description: res?.data?.fetchPoiCoordinates!,
+                    title: "Super ! 👍",
+                    backgroundColor: "green",
+                    icon: checkIcon,
+                });
+                /*  console.log("log de toastData au click après le set", toastData); */
+            })
+            .catch((erreur: ApolloError) => {
+                /* console.log(erreur); */
+                setToastData({
+                    id: Math.floor(Math.random() * 100 + 1),
+                    description: erreur.message,
+                    title: "Oups... 🧐",
+                    backgroundColor: "#bd2424",
+                    icon: errorIcon,
+                });
+            })
+            .finally(() => {
+                setShowToast(true);
+                refetch();
+            });
+    };
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Nom ou Adresse du POI"
-        value={poiRequested.poiNameOrAdress}
-        onChange={(e) =>
-          setPoiRequested((prevState) => ({
-            ...prevState,
-            poiNameOrAdress: e.target.value,
-            cityId: cityId,
-            cityName: cityName,
-          }))
-        }
-      ></input>
-      <button onClick={onClickSendNewPoi} className={"tertiaryButton"}>
-        Ajouter
-      </button>
-      <Toast
-        toast={toastData}
-        position={"bottomRight"}
-        autoDelete={true}
-        autoDeleteTime={5000}
-        visible={showToast}
-        setVisible={setShowToast}
-      />
-    </>
-  );
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Nom ou Adresse du POI"
+                value={poiRequested.poiNameOrAdress}
+                onChange={(e) =>
+                    setPoiRequested((prevState) => ({
+                        ...prevState,
+                        poiNameOrAdress: e.target.value,
+                        cityId: cityId,
+                        cityName: cityName,
+                    }))
+                }
+            ></input>
+            <button onClick={onClickSendNewPoi} className={"tertiaryButton"}>
+                Ajouter
+            </button>
+            <Toast
+                toast={toastData}
+                position={"bottomRight"}
+                autoDelete={true}
+                autoDeleteTime={5000}
+                visible={showToast}
+                setVisible={setShowToast}
+            />
+        </div>
+    );
 }
